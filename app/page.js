dfa6b616abc246d989ac29516edb7cf06fe23436
h01122742618-sudo <h01122742618@gmail.com>
2026-06-01 02:23:35 +0300

    Create Home component for Safari Limo booking

---
+++ app/page.js
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main style={{ textAlign: "center", padding: "50px" }}>
+      <h1>🚖 Safari Limo</h1>
+      <p>Luxury Limousine Booking System</p>
+
+      <button style={{ padding: "10px 20px", marginTop: "20px" }}>
+        Book Now
+      </button>
+    </main>
+  );
+  }
